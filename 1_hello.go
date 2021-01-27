3b02c40b1c92e7c084bde1f369b0c0b2e27964c9
Pedro Blöss Braga <50064364+pedroblossbraga@users.noreply.github.com>
2021-01-27 11:30:51 -0300

    Update 1_hello.go

--- 1_hello.go
+++ 1_hello.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt" //format
+import "fmt" 
 
 func main() {
-	var x string = "Hello"
-	endereco := &x
-	dados := *endereco
-
-	var ip *int
-	// fmt.Println(x)
-	fmt.Printf("x: %s\n", x)
-	fmt.Printf("endereco: %s\n", endereco)
-	fmt.Printf("dados: %s\n", dados)
-	fmt.Printf("ip: %s\n", ip)
+	fmt.Println("Hello World!")
 }
